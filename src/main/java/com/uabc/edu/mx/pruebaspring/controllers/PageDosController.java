f0549e616d2d3265310387c26cc7f35d217c3147
Xhendor <rosendorsc@gmail.com>
2019-02-21 19:58:36 -0800

    Ejemplo de clase

--- src/main/java/com/uabc/edu/mx/pruebaspring/controllers/PageDosController.java
+++ src/main/java/com/uabc/edu/mx/pruebaspring/controllers/PageDosController.java
@@ -1,7 +1,9 @@
 package com.uabc.edu.mx.pruebaspring.controllers;
 
 
+import com.uabc.edu.mx.pruebaspring.model.Gato;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PageDosController {
 
     @GetMapping(value = "/recurso")
-    public String pagina(){
+    public String pagina(Model modelo){
+
+        Gato donGato=new Gato();
+        donGato.setEdad(50f);
+        donGato.setNombre("DonGato");
+        donGato.setRaza("Siames");
+        donGato.setVivo(true);
+        modelo.addAttribute("myGato",donGato);
+
         return "pagina";
     }
 }
